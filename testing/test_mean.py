a30847d9d447f0318ee69e3799bbec3219b66941
raddaddy <46899887+raddaddy@users.noreply.github.com>
2019-05-05 19:24:55 +0200

    Corrections to test_mean.py

--- testing/test_mean.py
+++ testing/test_mean.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from ie_pandas import dataframe
 
 
@@ -17,23 +16,3 @@ def test_mean_numbers():
 
 
 test_mean_numbers()
-=======
-from ie_pandas import dataframe
-
-
-def test_mean_numbers():
-
-    dictionary = {"column 1": [1, 2, 3, 4],
-                  "column 2": [5, 6, 7, 8]}
-
-    df = dataframe(dictionary)
-
-    answer = df.mean()
-
-    expected_answer = [2.5, 6.5]
-
-    assert answer == expected_answer
-
-
-test_mean_numbers()
->>>>>>> bd443b3af94062a0f25def315b70dd2ed201fec8
