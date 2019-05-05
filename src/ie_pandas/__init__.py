9efd0adeb4be14f949c71b744998681f799dce72
smonteroparis <smonteroparis@icloud.com>
2019-05-05 17:32:06 +0200

    styling

--- src/ie_pandas/__init__.py
+++ src/ie_pandas/__init__.py
@@ -61,18 +61,12 @@ class dataframe:
     @property
     def styling(self):
         '''
-        Defines behavior for when
-        an item is deleted (e.g. del self[key])
+        Vizualise the table
         '''
-        self.columns = list(self.dictionary.keys())
-        self.values = list(self.dictionary.values())
-        data = []
-        for column, value in zip(self.columns, self.values):
-            col_list = [column]
-            col_list.extend(value)
-            data.append(col_list)
-        result = np.column_stack(data)
-        return result
+        table = PrettyTable()
+        for key, val in sorted(self.dictionary.items()):
+            table.add_column(key, val)
+        print(table)
 
     def get_row(self, row_index):
 
